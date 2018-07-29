366cf1dbc2f1ad5ab10c7990323555cd93a0927d
me9hana <meghanagoli@gmail.com>
2018-07-29 23:30:05 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -11,6 +11,8 @@ def change_rainbow_colors
   rainbow_colors.delete_at(1)
   rainbow_colors.delete_at(2)
   rainbow_colors.push("red", "light red", "yellow", "light yellow")
+  rainbow_colors << "green"
+  rainbow_colors << "blue"
 end
 change_rainbow_colors
 
